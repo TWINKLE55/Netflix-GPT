8e3d81cf6ac9f1f34686dbdb3af3f8e9446d83ed
twinkle55 <twinkle.kushwaha2@gmail.com>
2024-06-28 02:27:32 +0530

    bug fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { CheckValid } from "../utils/CheckValid";
 import Header from "./Header";
 import {
@@ -7,16 +7,24 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/Firebase";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { Avtar_URL, BG_URL } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const checkUser = useSelector((store) => store.user);
   const Name = useRef(null);
   const Email = useRef(null);
   const Password = useRef(null);
   const [error, setError] = useState(null);
   const [isSignIn, setIsSignIn] = useState(true);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  useEffect(() => {
+    if (checkUser) {
+      navigate("/browse");
+    }
+  });
   const handleValid = () => {
     const message = CheckValid(Email.current.value, Password.current.value);
     setError(message);
@@ -85,58 +93,60 @@ const Login = () => {
 
   return (
     <>
-      <div className="absolute w-screen">
-        <Header />
-        <img
-          className="h-screen object-cover w-[100%]"
-          alt=""
-          aria-hidden="true"
-          data-uia="nmhp-card-hero+background+image"
-          src={BG_URL}
-        ></img>
-      </div>
-      <div className="absolute mx-auto right-0 left-0 my-36 p-12 bg-black   w-[70%]  md:w-3/12 opacity-85 rounded-lg text-white">
-        <h1 className="text-3xl my-3 font-bold  ">
-          {isSignIn ? "Sign In" : "Sign Up"}
-        </h1>
-        <form onSubmit={(e) => e.preventDefault()}>
-          {!isSignIn && (
+      <div className=" w-screen h-screen">
+        <div className="absolute w-[100%] h-[100%] object-cover">
+          <Header />
+          <img
+            className="h-screen object-cover w-[100%]"
+            alt=""
+            aria-hidden="true"
+            data-uia="nmhp-card-hero+background+image"
+            src={BG_URL}
+          ></img>
+        </div>
+        <div className="absolute mx-auto right-0 left-0 my-36 p-12 bg-black   w-[70%]  md:w-3/12 opacity-85 rounded-lg text-white">
+          <h1 className="text-3xl my-3 font-bold  ">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </h1>
+          <form onSubmit={(e) => e.preventDefault()}>
+            {!isSignIn && (
+              <input
+                ref={Name}
+                className=" w-full rounded-lg my-3 p-3 bg-gray-900 "
+                type="text"
+                placeholder="Full Name"
+                required
+              />
+            )}
             <input
-              ref={Name}
+              ref={Email}
               className=" w-full rounded-lg my-3 p-3 bg-gray-900 "
-              type="text"
-              placeholder="Full Name"
+              type="email"
+              placeholder="Email Address"
               required
             />
-          )}
-          <input
-            ref={Email}
-            className=" w-full rounded-lg my-3 p-3 bg-gray-900 "
-            type="email"
-            placeholder="Email Address"
-            required
-          />
-          <input
-            ref={Password}
-            className=" w-full rounded-lg my-3 p-3  bg-gray-900 "
-            type="password"
-            placeholder="Password"
-            required
-          />
-          <p className=" text-red-700">{error}</p>
-          <button
-            onClick={handleValid}
-            className=" w-full rounded-lg my-3 p-3 text-lg font-bold bg-red-700"
-            typeof="submit"
-          >
-            {isSignIn ? "Sign In" : "Sign Up"}
-          </button>
-          <p className="cursor-pointer" onClick={handleSignIn}>
-            {isSignIn
-              ? "New to Netflix? Sign Up Now."
-              : "Already a User.Sign In Now."}
-          </p>
-        </form>
+            <input
+              ref={Password}
+              className=" w-full rounded-lg my-3 p-3  bg-gray-900 "
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <p className=" text-red-700">{error}</p>
+            <button
+              onClick={handleValid}
+              className=" w-full rounded-lg my-3 p-3 text-lg font-bold bg-red-700"
+              typeof="submit"
+            >
+              {isSignIn ? "Sign In" : "Sign Up"}
+            </button>
+            <p className="cursor-pointer" onClick={handleSignIn}>
+              {isSignIn
+                ? "New to Netflix? Sign Up Now."
+                : "Already a User.Sign In Now."}
+            </p>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/utils/lang.js
+++ src/utils/lang.js
@@ -2,14 +2,20 @@ const lang = {
   en: {
     search: "Search",
     placeholder: "what's in your mind? let's do NETFLIX and CHILL Today.",
+    cast: "Cast",
+    similarMovies: "similarMovies",
   },
   hindi: {
     search: "खोज",
     placeholder: "आपके दिमाग में क्या है? चलो आज नेटफ्लिक्स और चिल करते हैं।",
+    cast: "कलाकार",
+    similarMovies: "इसी तरह की फिल्में",
   },
   spanish: {
     search: "Buscar",
     placeholder: "¿que estás pensando? Hagamos NETFLIX y CHILL hoy.",
+    cast: "elenco",
+    similarMovies: "Películas similares",
   },
 };
 export default lang;
